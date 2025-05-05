28c1a425117f23a60fc12bbac83853308e703b23
japygo <hyunnjinn@gmail.com>
2025-05-05 15:55:30 +0900

    좋아요 수 구현

--- service/like/src/main/java/kuke/board/like/repository/ArticleLikeRepository.java
+++ service/like/src/main/java/kuke/board/like/repository/ArticleLikeRepository.java
@@ -3,6 +3,8 @@ package kuke.board.like.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import kuke.board.like.entity.ArticleLike;
@@ -10,4 +12,11 @@ import kuke.board.like.entity.ArticleLike;
 @Repository
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
     Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+
+    @Query(
+        value = "delete from article_like where article_id = :articleId and user_id = :userId",
+        nativeQuery = true
+    )
+    @Modifying
+    int deleteByArticleIdAndUserId(Long articleId, Long userId);
 }
