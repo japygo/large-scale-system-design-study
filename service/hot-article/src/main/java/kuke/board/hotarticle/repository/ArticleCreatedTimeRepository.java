0d06315427fe2fd0fc5b4bd2967f27626fbb5a6a
japygo <hyunnjinn@gmail.com>
2025-05-07 22:45:23 +0900

    인기글 Consumer 구현 - 이벤트 데이터 리포지토리

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/repository/ArticleCreatedTimeRepository.java
@@ -0,0 +1,50 @@
+package kuke.board.hotarticle.repository;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleCreatedTimeRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    // hot-article::article::{articleId}::created-time
+    private static final String KEY_FORMAT = "hot-article::article::%s::created-time";
+
+    public void createOrUpdate(Long articleId, LocalDateTime createdTime, Duration ttl) {
+        redisTemplate.opsForValue().set(
+            generateKey(articleId),
+            String.valueOf(createdTime.toInstant(ZoneOffset.UTC).toEpochMilli()),
+            ttl
+        );
+
+        // 좋아요 이벤트가 왔는데, 이 이벤트에 대한 게시글이 오늘 게시글인지 확인하려면, 게시글 서비스에 조회가 필요합니다.
+        // 하지만 게시글 생성 시간을 저장하고 있으면, 오늘 게시글인지 게시글 서비스 안찔러봐도 바로 알 수 있습니다.
+    }
+
+    public void delete(Long articleId) {
+        redisTemplate.delete(generateKey(articleId));
+    }
+
+    public LocalDateTime read(Long articleId) {
+        String result = redisTemplate.opsForValue().get(generateKey(articleId));
+        if (result == null) {
+            return null;
+        }
+        return LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(Long.parseLong(result)), ZoneOffset.UTC
+        );
+    }
+
+    private String generateKey(Long articleId) {
+        return KEY_FORMAT.formatted(articleId);
+    }
+
+}
