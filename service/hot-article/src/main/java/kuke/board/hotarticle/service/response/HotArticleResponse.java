6b9b41a6520546623d740a9734795b87b97e0097
japygo <hyunnjinn@gmail.com>
2025-05-07 23:50:31 +0900

    인기글 Consumer 구현 - 이벤트 핸들러 및 서비스 레이어

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/service/response/HotArticleResponse.java
@@ -0,0 +1,21 @@
+package kuke.board.hotarticle.service.response;
+
+import java.time.LocalDateTime;
+
+import kuke.board.hotarticle.client.ArticleClient;
+import lombok.Getter;
+
+@Getter
+public class HotArticleResponse {
+    private Long articleId;
+    private String title;
+    private LocalDateTime createdAt;
+
+    public static HotArticleResponse from(ArticleClient.ArticleResponse articleResponse) {
+        HotArticleResponse hotArticleResponse = new HotArticleResponse();
+        hotArticleResponse.articleId = articleResponse.getArticleId();
+        hotArticleResponse.title = articleResponse.getTitle();
+        hotArticleResponse.createdAt = articleResponse.getCreatedAt();
+        return hotArticleResponse;
+    }
+}
